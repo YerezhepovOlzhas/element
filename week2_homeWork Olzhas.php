b185e95fc5780c95d702b07356486fa66665d9d1
YerezhepovOlzhas <118345473+YerezhepovOlzhas@users.noreply.github.com>
2022-11-21 12:06:56 +0600

    Create week2_homeWork Olzhas.php

---
+++ week2_homeWork Olzhas.php
@@ -0,0 +1,126 @@
+//<Задание 1>
+
+//var_dump(round(3.65, 1));
+
+//echo "<br>";
+
+//$number = 6.65;
+//var_dump(round($number, 1));
+
+//echo "<br>";
+
+//var_dump(round(-11.54, 1));
+
+
+  
+//<Задание 2>
+//echo "<br>Цифра: ";
+//echo ($a = rand(10, 1000));
+//echo "<br>Цифра: ";
+//echo ($b = rand(10, 1000));
+//
+//
+//const addition = '1'; //сложение
+//const subtraction = '2'; //вычитание
+//const multiplication = '3'; //умножение
+//const division = '4'; //деление
+//const modul = '5'; //деление по модулю
+//
+//$c=rand(1,5);
+//echo "<br>";
+//
+//echo "Функция: $c<br>";
+//
+//function operator ($a,$b,$c)
+//{
+//    if ($c == addition) {
+//        $result = $a + $b;
+//        echo "Результат: {$a}+{$b}={$result}<br>";
+//    } elseif ($c == subtraction) {
+//        $result = $a - $b;
+//        echo "Результат: {$a}-{$b}={$result}<br>";
+//    } elseif ($c == multiplication) {
+//        $result = $a * $b;
+//        echo "Результат: {$a}*{$b}={$result}<br>";
+//    } elseif ($c == division) {
+//        $result = $a / $b;
+//        echo "Результат: {$a}/{$b}={$result}<br>";
+//    } elseif ($c == modul) {
+//        $result = $a % $b;
+//        echo "Результат: {$a}%{$b}={$result}<br>";
+//    }
+//}
+//operator($a,$b,$c);
+
+
+  
+  
+  
+//< Задание 3 >
+//$a = rand(1,3); //Акерке
+//$b = rand(1,3); //Анна
+//const ROCK = 1;
+//const PAPER = 2;
+//const SCISSORS = 3;
+//
+//
+//
+//function operator ($a, $b) {
+//
+//    if ($a == ROCK and $b == PAPER) {
+//
+//        echo "Акерке: Камень<br>  Анна: Бумага<br>  Выиграл игрок: Анна<br> ";
+//
+//    } elseif ($a == ROCK and $b == SCISSORS) {
+//
+//        echo "Акерке: Камень<br>  Анна: Ножницы<br>  Выиграл игрок: Акерке<br>";
+//
+//    } elseif ($a == PAPER and $b == ROCK) {
+//
+//        echo "Акерке: Бумага<br>  Анна: Камень<br>  Выиграл игрок: Акерке<br>";
+//
+//    } elseif ($a == PAPER and $b == SCISSORS) {
+//
+//        echo "Акерке: Бумага<br>  Анна: Ножницы<br>  Выиграл игрок: Анна<br>";
+//
+//    } elseif ($a == SCISSORS and $b == ROCK) {
+//
+//        echo "Акерке: Ножницы<br>  Анна: Камень<br>  Выиграл игрок: Анна<br>";
+//
+//    } elseif ($a == SCISSORS and $b == PAPER) {
+//
+//        echo "Акерке: Ножницы<br>  Анна: Бумага<br>  Выиграл игрок: Акерке<br>";
+//
+//    } elseif ($a == SCISSORS and $b == SCISSORS) {
+//
+//        echo "Акерке: Ножницы<br>  Анна: Ножницы<br>  Выиграл игрок: Ничья<br>";
+//
+//    } elseif ($a == ROCK and $b == ROCK) {
+//
+//        echo "Акерке: Камень<br>  Анна: Камень<br>  Выиграл игрок: Ничья<br>";
+//
+//    } elseif ($a == PAPER and $b == PAPER){
+//
+//        echo "Акерке: Бумага<br>  Анна: Бумага<br>  Выиграл игрок: Ничья<br>";
+//       }
+//
+//}
+//
+//
+//     operator($a,$b);
+
+
+  
+  
+// < Задание 4 >
+
+//   $a = rand(1, 10);
+//   $b = rand(1, 10);
+//
+//   echo "Было A = {$a}, B = {$b}<br>";
+//
+//   $a = $a + $b;
+//   $b = $a - $b;
+//   $a = $a - $b;
+//
+//   echo "Стало A = {$a}, B = {$b}";
